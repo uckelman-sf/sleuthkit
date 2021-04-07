8434239cd5ad1349ba95c46b8102bc818689f662
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2021-04-07 16:36:28 +0100

    Fixes for MinGW builds.

--- .world/config.sh
+++ .world/config.sh
@@ -9,6 +9,7 @@ if [ $Target = 'windows' ]; then
     for i in base img ; do
       ln -snf -t unit_tests/$i $(realpath -m $INSTALL/bin/*.dll tsk/.libs/libtsk-19.dll)
     done
+    ln -snf -t tests $(realpath -m $INSTALL/bin/*.dll tsk/.libs/libtsk-19.dll)
   fi
 fi
 
