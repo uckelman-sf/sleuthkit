41d60bef275d5d41527ee18cb791c002bd1e6de9
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2022-01-10 14:31:08 +0000

    Link with -fstack-protector for Windows.

--- .world/config.sh
+++ .world/config.sh
@@ -5,6 +5,8 @@
 CONF_FLAGS+=" --disable-java --without-afflib --with-libaff4 --with-libewf --with-libqcow --with-libvhdi --with-libvmdk"
 
 if [ $Target = 'windows' ]; then
+  LDFLAGS+=' -fstack-protector'
+
   if [ $Linkage = 'shared' ]; then
     for i in base img ; do
       ln -snf -t unit_tests/$i $(realpath -m $INSTALL/bin/*.dll tsk/.libs/libtsk-19.dll)
