b3664d910dc8ee5979cdd4158a319fae5e0f4b1a
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2023-09-04 17:07:07 +0100

    B - The test structure is such that we need an absolute path for wine_wrapper.sh.

--- .world/build.sh
+++ .world/build.sh
@@ -5,7 +5,7 @@
 make_it
 
 if [ "$Target" = 'windows' ]; then
-  MAKE_FLAGS+=' LOG_COMPILER=.world/wine_wrapper.sh'
+  MAKE_FLAGS+=" LOG_COMPILER=$(realpath .world/wine_wrapper.sh)"
 fi
 
 make_check_it
