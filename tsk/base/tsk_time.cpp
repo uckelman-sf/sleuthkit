99e35cb477882900a332d648492ebfe07383e2d5
Joel Uckelman <joel.uckelman@aon.com>
2024-08-21 12:14:34 +0100

    F - Use std::chrono TZDB support instead of libtz. Requires C++20.

--- tsk/base/tsk_time.cpp
+++ tsk/base/tsk_time.cpp
@@ -3,22 +3,19 @@
 #include <cstring>
 #include <ctime>
 
-#include <date/date.h>
-#include <date/tz.h>
-
 #include "tsk_base_i.h"
 
 struct tm* tsk_localtime(const time_t* tt) {
-#if defined(TSK_WIN32) && defined(HAVE_LIBDATE_TZ)
+#if defined(TSK_WIN32)
   // determine our local timezone
   const char* tzenv = std::getenv("TZ");
-  const auto tz = tzenv ? date::locate_zone(tzenv) : date::current_zone();
+  const auto tz = tzenv ? std::chrono::locate_zone(tzenv) : std::chrono::current_zone();
 
   // get a time_point from the time_t
   const auto tp = std::chrono::system_clock::from_time_t(*tt);
 
   // convert the time_point to the local timezone
-  const auto zt = date::make_zoned(tz, tp);
+  const auto zt = std::chrono::zoned_time{tz, tp};
   const auto lt = zt.get_local_time();
 
   // localtime uses a static tm; we make it thread-safe
@@ -26,9 +23,9 @@ struct tm* tsk_localtime(const time_t* tt) {
   std::memset(&t, 0, sizeof(t));
 
   // break up the local time_point into components
-  const auto dp = date::floor<date::days>(lt);
-  const date::year_month_day ymd{dp};
-  const auto hms = date::make_time(lt-dp);
+  const auto dp = std::chrono::floor<std::chrono::days>(lt);
+  const std::chrono::year_month_day ymd{dp};
+  const auto hms = std::chrono::hh_mm_ss(lt-dp);
 
   // fill the tm with the components
   t.tm_year = static_cast<int>(ymd.year()) - 1900;
@@ -46,24 +43,26 @@ struct tm* tsk_localtime(const time_t* tt) {
 }
 
 time_t tsk_mktime(struct tm* t) {
-#if defined(TSK_WIN32) && defined(HAVE_LIBDATE_TZ)
+#if defined(TSK_WIN32)
   // POSIX.1 8.1.1 requires that mktime() behave as though tzset() has been
   // called; so we call tzset() to ensure that.
   tzset();
 
   // determine our local timezone
   const char* tzenv = std::getenv("TZ");
-  const auto tz = tzenv ? date::locate_zone(tzenv) : date::current_zone();
+  const auto tz = tzenv ? std::chrono::locate_zone(tzenv) : std::chrono::current_zone();
 
   // create a local time
+  const auto date = std::chrono::year{t->tm_year + 1900} / (t->tm_mon + 1) / t->tm_mday;
+
   const auto lt =
-    date::local_days{date::year{t->tm_year+1900}/(t->tm_mon+1)/t->tm_mday} +
+    std::chrono::local_days{date} +
     std::chrono::hours{t->tm_hour} +
     std::chrono::minutes{t->tm_min} +
     std::chrono::seconds{t->tm_sec};
 
   // attach a time zone to the local time
-  const auto zt = date::make_zoned(tz, lt);
+  const auto zt = std::chrono::zoned_time{tz, lt};
   t->tm_isdst = zt.get_info().save != std::chrono::minutes(0);
 
   // convert the local time to system time
