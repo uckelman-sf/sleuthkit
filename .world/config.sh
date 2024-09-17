3412603203afb532b5b10d953c5e3c759faf1976
Joel Uckelman <joel.uckelman@aon.com>
2024-09-17 17:18:14 +0100

    F - Updated path to reflect current state of Sleuthkit.

--- .world/config.sh
+++ .world/config.sh
@@ -8,9 +8,7 @@ if [ $Target = 'windows' ]; then
   LDFLAGS+=' -fstack-protector'
 
   if [ $Linkage = 'shared' ]; then
-    for i in base img ; do
-      ln -snf -t unit_tests/$i $(realpath -m $INSTALL/bin/*.dll tsk/.libs/libtsk-19.dll)
-    done
+    ln -snf -t unit_tests $(realpath -m $INSTALL/bin/*.dll tsk/.libs/libtsk-19.dll)
     ln -snf -t tests $(realpath -m $INSTALL/bin/*.dll tsk/.libs/libtsk-19.dll)
   fi
 fi
